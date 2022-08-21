6f52512d0942aaa0a66efa686f6b5bcf946865c5
Ruben Garcia <rubengarciarbngrc@gmail.com>
2022-08-21 23:05:36 +0200

    avoiding infinit loop in points

--- client/src/actions/points.js
+++ client/src/actions/points.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Axios from 'axios';
 
 import { url } from '../constans';
@@ -7,26 +7,28 @@ export const usePoints = (initialState = []) => {
 
   const [athleteList, setAthleteList] = useState([]);
 
-  const getAthletes = async (name) => {
-    const {data:res} = await Axios.get(url + 'atletas');
-        setAthleteList(res);
-      };
-      getAthletes();
+  useEffect(() => {
+    const getAthletes = async (name) => {
+        const {data:res} = await Axios.get(url + 'atletas');
+            setAthleteList(res);
+          };
+          getAthletes();
+    }, []);
 
-    athleteList.map((val, key) => {
-      const name = val.name
-      return console.log(name + " normal")
-    }) 
+  athleteList.map((val, key) => {
+    const name = val.name
+    return console.log(name + " normal")
+  }) 
 
-    athleteList.reverse().map((val, key) => {
-      const name = val.name
-      return console.log(name + " al reves")
-    }) 
+  athleteList.reverse().map((val, key) => {
+    const name = val.name
+    return console.log(name + " al reves")
+  }) 
 
-    for (let i = 0; i < athleteList.length; i++) {
-      const element = i;
-      console.log(element)
-    }
-    return athleteList
-    
-  } 
+  for (let i = 0; i < athleteList.length; i++) {
+    const element = i;
+    console.log(element)
+  }
+  return athleteList
+
+} 
