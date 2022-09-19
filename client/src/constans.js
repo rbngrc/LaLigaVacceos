2e9291900e74c2d2605539bbd7beddc87f9963ce
Ruben Garcia <rubengarciarbngrc@gmail.com>
2022-09-19 21:08:26 +0200

    small changes

--- client/src/constans.js
+++ client/src/constans.js
@@ -1,9 +1,9 @@
 /********** Constantes de Produccion **********/
 
-// export const url = "https://vacceos-liga.herokuapp.com/";
+export const url = "https://vacceos-liga.herokuapp.com/";
 
 /********** Constantes de Local **********/
-export const url = "http://localhost:3001/";
+// export const url = "http://localhost:3001/";
 
 /********** Constantes de Mensajes **********/
 export const consultaVacia = "En espera";
